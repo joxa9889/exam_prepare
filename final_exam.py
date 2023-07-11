528ff5cc550c19581b2b022519d9fe4d01551886
Joxa2008 <jovoooi2008@gmail.com>
2023-07-11 18:39:11 +0500

    Holla

---
+++ final_exam.py
@@ -0,0 +1,45 @@
+"""
+1. Range funsiyasi nima va u qanday parametorlari bor ?
+"""
+
+# Range funksiyasi ma'lum oraliqdagi sonlarni yaratish uchun ishlatiladi.
+# Va unda uchta parametorlari bor va ulaarning nomalri START, STOP, STEP, 
+# Start ning defaulti 0 bolib uni ozgartirish mumku start malum bir sondan boshlab sanash uchun beriladi
+# Stop bu majburiy parametor bolib startdan boshlab malum bir raqamlargachan bolgan raqamlar yaratiladi
+# Step bu malum bir son orqali qadam berasiz va shu reaqam boylab sanaydi masalan stepga 3 bersangiz u startdan boshlab stopgacha uchta raqam tashlab sanaydi
+
+
+"""
+2. Pythonning asosiy xususiyatlari qanday?
+"""
+
+# Python - mashhur dasturlash tili. U Guido van Rossum tomonidan yaratilgan va 1991 yilda chiqarilgan.
+
+# Python deyarliy hamma ooperatsiyon sistamalarida ishlaydi massaln (Windows, Mac, Linux, Respberry Pi, va hokazo)
+# Python ingliz tiliga o'xshash oddiy sintaksisga ega.
+# Python interpritator tizimida ishlaydi, ya'ni kod yozilishi bilanoq bajarilishi mumkin. Bu prototip yaratish juda tez bo'lishi mumkinligini anglatadi.
+# Python functsiyonal va OOP yonalishida ishlashi mumkin
+
+"""
+3. Python moduli nima
+"""
+
+# Proektizga kiritmoqchi bo'lgan funktsiyalar to'plamini o'z ichiga olgan fayl.
+# Modulni hamma funcsiylarini faylda ishlatish uchun importni ishlatish kerag
+# Modulni faylda ishlatayotkanda modulni ismni yozim nuxta qoyib kegin modldagi keragli funksiyani nomini yozish kerag
+
+"""
+4. Gitni qanday ishlatadi?
+"""  
+
+# Git - bu bepul, ochiq manbali, Linus Torvalds tomonidan 2005 yilda yaratilgan versiyani boshqarish tizimi yoki talqinlarni boshqarish tizimi.
+# VCS asosan vaqt o'tishi bilan bir yoki bir nechta fayl ichidagi o'zgarishlarni yozib olish uchun mo'ljallangan dasturdir.
+
+"""
+5. 'is' bilan '==' farqi nimada? misol keltiring
+"""
+l1 = [12]
+l2 = [12]
+print(l1 is l2)
+
+#
